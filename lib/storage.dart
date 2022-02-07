1e9cf0b24372f5b34c8f9e7c6a8738686bcf3441
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-02-07 11:51:40 -0500

    Use globalStorage database for source of recent workspaces
    VSCode recently moved the storage for the recent workspaces,
    it is now in the globalStorage `state.vscdb` file.
    
    https://github.com/microsoft/vscode/issues/138072
    
    This update sources this database to fix compatability with
    the new version of VSCode (1.64).

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-/// Represents the `storage.json` file that VSCode uses for configurations.
+/// Represents the `state.vscdb` file that VSCode uses for configurations.
 class Storage {
   /// A list of all the recently opened paths VSCode remembers.
   final List<String> recentPaths;
@@ -9,10 +9,10 @@ class Storage {
     required this.recentPaths,
   });
 
-  /// Generate the [recentPaths] from the `storage.json` data.
+  /// Generate the [recentPaths] from the `state.vscdb` database.
   static Storage fromJson(String data) {
     final dataMap = json.decode(data) as Map<String, dynamic>;
-    final rawPathList = dataMap['openedPathsList']['entries'] as List;
+    final rawPathList = dataMap['entries'] as List;
     final pathMaps = rawPathList.cast<Map<String, dynamic>>();
     final paths =
         pathMaps.map((e) => e['folderUri'] ?? '').toList().cast<String>();
