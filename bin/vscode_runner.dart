f56ece19ace826771bacd7e2499b1c802b4fa8a1
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-09-26 13:18:10 -0400

    feat: add logging to assist debugging user issues

--- bin/vscode_runner.dart
+++ bin/vscode_runner.dart
@@ -2,13 +2,16 @@ import 'dart:io';
 
 import 'package:krunner/krunner.dart';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:vscode_runner/logs/logging_manager.dart';
 import 'package:vscode_runner/storage.dart';
 import 'package:xdg_directories/xdg_directories.dart';
 
 Future<void> main(List<String> arguments) async {
   await checkIfAlreadyRunning();
+  await LoggingManager.initialize(verbose: true);
 
-  print('Starting VSCode runner.');
+  log.i('Starting VSCode runner.');
+  await checkVSCodeExePath();
 
   final runner = KRunnerPlugin(
     identifier: 'codes.merritt.vscode_runner',
@@ -28,16 +31,30 @@ Future<void> checkIfAlreadyRunning() async {
   final result = await Process.run('pidof', ['vscode_runner']);
   final hasError = result.stderr != '';
   if (hasError) {
-    print('Issue checking for existing process: ${result.stderr}');
+    log.e('Issue checking for existing process: ${result.stderr}');
     return;
   }
   final output = result.stdout as String;
   final runningInstanceCount = output.trim().split(' ').length;
   if (runningInstanceCount != 1) {
-    print('An instance of vscode_runner appears to already be running. '
+    log.e('An instance of vscode_runner appears to already be running. '
         'Aborting run of new instance.');
     exit(0);
   }
+}
+
+Future<void> checkVSCodeExePath() async {
+  final result = await Process.run('which', ['code']);
+  final hasError = result.stderr != '';
+
+  if (hasError) {
+    log.e('Unable to locate code executable: ${result.stderr}');
+    return;
+  }
+
+  final output = result.stdout as String;
+  final path = output.trim();
+  log.i('Found VSCode executable at $path');
 }
 
 /// Load VSCode's globalStorage database and retrieve recent paths.
@@ -48,10 +65,14 @@ List<String> fetchPaths() {
   try {
     db = sqlite3.open(dbFile, mode: OpenMode.readOnly);
   } catch (e) {
-    print('Unable to open VSCode database file.\n'
-        '$e');
+    log.e('Unable to open VSCode database file.\n'
+        'Expected file at $dbFile\n'
+        'Make sure sqlite3 is installed.\n'
+        'Error: $e');
     return const [];
   }
+
+  log.i('Opened VSCode database file at $dbFile');
 
   final rows = db.select(
     "SELECT value FROM ItemTable WHERE key = 'history.recentlyOpenedPathsList'",
@@ -65,6 +86,7 @@ List<String> fetchPaths() {
 }
 
 Future<List<QueryMatch>> matchQuery(String query) async {
+  log.i('Running query for: $query');
   final recentWorkspacePaths = fetchPaths();
   recentWorkspacePaths.removeWhere((element) => !element.contains(query));
 
@@ -73,7 +95,7 @@ Future<List<QueryMatch>> matchQuery(String query) async {
     final relativePath = parseRelativePath(uri);
     final projectName = path.split('/').last;
     return QueryMatch(
-      id: path,  // id is the raw folderUri starting with file:// or vscode-remote://
+      id: path, // id is the raw folderUri starting with file:// or vscode-remote://
       title: projectName,
       icon: 'com.visualstudio.code',
       rating: QueryMatchRating.exact,
@@ -87,7 +109,9 @@ Future<List<QueryMatch>> matchQuery(String query) async {
 
 Uri pathToUri(String path) {
   // Remove the leading "file://" or "vscode-remote://"
-  final trimmed = path.replaceFirst('file://', '').replaceFirst('vscode-remote://', '');
+  final trimmed = path
+      .replaceFirst('file://', '') //
+      .replaceFirst('vscode-remote://', '');
   final uri = Uri.directory(trimmed);
   return uri;
 }
@@ -135,6 +159,8 @@ Future<void> runAction({
   required String actionId,
   required String matchId,
 }) async {
+  log.i('Running action. actionId: $actionId, matchId: $matchId');
+
   final isOpenFolderRequest =
       (dbusToString(actionId) == "openContainingFolder") ? true : false;
   final path = dbusToString(matchId);
@@ -148,11 +174,14 @@ Future<void> runAction({
 }
 
 Future<void> openWorkspace(String path) async {
+  log.i('Opening workspace at $path');
+
   // just pass the raw path as --folder-uri, VSCode will handle it
   // see https://stackoverflow.com/questions/60144074/how-to-open-a-remote-folder-from-command-line-in-vs-code
   await Process.run('code', ['--folder-uri=' + path]);
 }
 
 Future<void> openContainingFolder(String path) async {
+  log.i('Opening containing folder at $path');
   await Process.run('xdg-open', [path]);
 }
