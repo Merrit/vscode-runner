088ada1016170a926c16eb6bd38e8f9e4bb64ab1
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-11-09 12:53:16 -0500

    refactor: move all db related code to database.dart

---
+++ lib/database.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:sqlite3/sqlite3.dart';
+
+import 'logs/logging_manager.dart';
+
+/// Represents the database from the `state.vscdb` file that VSCode uses for
+/// configurations.
+class VSCodeDatabase {
+  final String _dbFilePath;
+
+  const VSCodeDatabase(this._dbFilePath);
+
+  /// Returns a list where each is the path of a workspace that is remembered
+  /// by VSCode, in its "recent workspaces" list.
+  List<String> getRecentWorkspacePaths() {
+    final Database db;
+
+    try {
+      db = sqlite3.open(_dbFilePath, mode: OpenMode.readOnly);
+    } catch (e) {
+      log.e('Unable to open VSCode database file.\n'
+          'Expected file at $_dbFilePath\n'
+          'Make sure sqlite3 is installed.\n'
+          'Error: $e');
+      return const [];
+    }
+
+    log.i('Opened VSCode database file at $_dbFilePath');
+
+    final rows = db.select(
+      "SELECT value FROM ItemTable WHERE key = 'history.recentlyOpenedPathsList'",
+    );
+    db.dispose();
+
+    final jsonString = rows.first.values.first as String;
+    final recentPaths = _recentPathsFromJson(jsonString);
+
+    return recentPaths;
+  }
+
+  /// Converts the json from the db query into a `List<String>` of paths.
+  List<String> _recentPathsFromJson(String data) {
+    final dataMap = json.decode(data) as Map<String, dynamic>;
+    final rawPathList = dataMap['entries'] as List;
+    final pathMaps = rawPathList.cast<Map<String, dynamic>>();
+    final paths =
+        pathMaps.map((e) => e['folderUri'] ?? '').toList().cast<String>();
+    return paths;
+  }
+}
